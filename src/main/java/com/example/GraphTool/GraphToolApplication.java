42a42fc9618eab6ec4fb17cab6925661608d3d34
kubec <kubecki.filip@gmail.com>
2023-07-28 14:27:38 +0200

    Initial commit. Spring initializr setup.

---
+++ src/main/java/com/example/GraphTool/GraphToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.GraphTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GraphTool/GraphToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GraphTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
